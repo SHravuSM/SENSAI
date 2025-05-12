188bb8e91c991bdb37b595313c8c3dab46b60d22
SHravankumar Muchchandi <shravankumarmuchchandi@gmail.com>
2025-05-12 21:30:31 +0530

    five

--- app/layout.js
+++ app/layout.js
@@ -35,11 +35,11 @@ export default function RootLayout({ children }) {
             <main className="min-h-screen">{children}</main>
             <Toaster richColors />
 
-            <footer className="bg-muted/50 py-12">
-              <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💗 by RoadsideCoder</p>
-              </div>
-            </footer>
+            {/* <footer className="bg-muted/50 py-12"> */}
+              {/* <div className="container mx-auto px-4 text-center text-gray-200"> */}
+                {/* <p>Made with 💗 by RoadsideCoder</p> */}
+              {/* </div> */}
+            {/* </footer> */}
           </ThemeProvider>
         </body>
       </html>
